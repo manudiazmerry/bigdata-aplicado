59a7e6b7b461eaef4686b3e28c62c4db7ace9a26
santi8445 <santi8445ssp@gmail.com>
2022-04-02 23:19:17 +0200

    commit Santiago

---
+++ spark/entrega_Spark-Kafka/spark_SGR/vendas_tenda.py
@@ -0,0 +1,56 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import from_json, col
+from pyspark.sql.types import StructType, StructField, StringType
+
+appName = "Streaming de Lectura JSON Kafka"
+master = "local"
+KAFKA_SERVERS = "kafkaserver:9092"
+TOPIC = "vendas"
+
+spark = SparkSession.builder \
+    .master(master) \
+    .appName(appName) \
+    .getOrCreate()
+
+# Fixar o nivel de rexistro/log a ERROR
+spark.sparkContext.setLogLevel("ERROR")
+
+# Stream de lectura
+df = spark \
+    .readStream \
+    .format("kafka") \
+    .option("kafka.bootstrap.servers", KAFKA_SERVERS) \
+    .option("subscribe", TOPIC) \
+    .load()
+
+df = df.selectExpr("CAST(value AS STRING)")
+
+# Defino o schema dos JSON codificado no campo value
+schema = StructType(
+    [
+        StructField('Tenda', StringType(), True),
+        StructField('Categoria', StringType(), True),
+        StructField('Produto', StringType(), True),
+	StructField('Prezo', StringType(), True)
+    ]
+)
+
+# Creo un novo dataframe no que extraio de value todos os campos en formato JSON
+df2 = df.withColumn("value", from_json("value", schema)).select(col('value.*'))
+
+
+df2.createOrReplaceTempView("datos")
+
+beneficios = spark.sql("select Tenda, count(*) as numero, sum(Prezo) as Beneficios from datos group by Tenda")
+
+
+# Stream de escritura
+query = beneficios \
+    .writeStream \
+    .outputMode("complete") \
+    .format("console") \
+    .option("truncate", False) \
+    .trigger(processingTime='5 seconds') \
+    .start()
+
+query.awaitTermination()
